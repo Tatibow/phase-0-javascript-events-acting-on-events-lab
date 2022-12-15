ce3f96d30df5d854b98a38d67a18488346e1a6c2
Tatiana <Bowmantatiana2@gmail.com>
2022-12-14 16:08:51 -0800

    commit tree 622a59f0d0d3a23a5a749cfd5e4e0dbcb57f7831

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${right + 1}px`;
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
